043636b14c75fe2c0f9dcb5a3ad7ec3b1f170119
ArchLeaders <archparliament@gmail.com>
2022-08-25 20:11:15 -0700

    Create Brush extension

---
+++ BotwAvaloniaTemplate/Extensions/BrushExt.cs
@@ -0,0 +1,19 @@
+﻿using Avalonia.Media;
+
+namespace BotwAvaloniaTemplate.Extensions
+{
+    public static class BrushExt
+    {
+        public static Brush ToBrush(this string color) => (Brush)(new BrushConverter().ConvertFromString(color) ?? new());
+        public static Brush? GetBrush(this string brush)
+        {
+            if (Avalonia.Application.Current != null) {
+                Avalonia.Application.Current.Resources.TryGetResource(brush, out object? obj);
+                return obj as Brush;
+            }
+            else {
+                return null;
+            }
+        }
+    }
+}
