dc1d87142e1f59aec652c4351c810c962844fa74
ArchLeaders <archparliament@gmail.com>
2022-08-25 20:11:36 -0700

    Create View extension

---
+++ BotwAvaloniaTemplate/Extensions/ViewExt.cs
@@ -0,0 +1,15 @@
+﻿using Avalonia.Controls;
+using System.Threading.Tasks;
+using BotwAvaloniaTemplate.Dialogs;
+
+namespace BotwAvaloniaTemplate.Extensions
+{
+    public enum MessageBoxButtons { Ok, OkCancel, YesNo, YesNoCancel }
+    public enum MessageBoxResult { Cancel, No, Ok, Yes }
+    public enum Formatting { None, Markdown }
+    public static class ViewExt
+    {
+        public static Task<MessageBoxResult> ShowMessageBox(this Window parent, string text, string title = "Warning", MessageBoxButtons buttons = MessageBoxButtons.Ok,
+            Formatting formatting = Formatting.None) => MessageBox.Show(text, title, buttons, parent, formatting);
+    }
+}
