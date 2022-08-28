4a4ad110ac04069432c9e6fdb9ad4f560bd06740
ArchLeaders <archparliament@gmail.com>
2022-08-28 14:33:40 -0700

    Load the proper dialog; rename mode enum

--- BotwAvaloniaTemplate/Extensions/ViewExt.cs
+++ BotwAvaloniaTemplate/Extensions/ViewExt.cs
@@ -8,21 +8,21 @@ namespace BotwAvaloniaTemplate.Extensions
     public enum MessageBoxButtons { Ok, OkCancel, YesNo, YesNoCancel }
     public enum MessageBoxResult { Cancel, No, Ok, Yes }
     public enum Formatting { None, Markdown }
-    public enum BrowserDialogType { File, Folder, FileFolder }
+    public enum BrowserDialog { File, Folder, FileFolder }
 
     public static class ViewExt
     {
         public static Task<MessageBoxResult> ShowMessageBox(this Window _, string text, string title = "Warning", MessageBoxButtons buttons = MessageBoxButtons.Ok,
             Formatting formatting = Formatting.None) => MessageBox.Show(text, title, buttons, formatting);
 
-        public static async Task<string?> BrowseDialog(this BrowserDialogType browser, string title = "")
+        public static async Task<string?> BrowseDialog(this BrowserDialog browser, string title = "")
         {
-            if (browser == BrowserDialogType.File) {
+            if (browser == BrowserDialog.Folder) {
                 OpenFolderDialog dialog = new() { Title = title };
                 var result = await dialog.ShowAsync(View);
                 return result;
             }
-            else if (browser == BrowserDialogType.Folder) {
+            else if (browser == BrowserDialog.File) {
                 OpenFileDialog dialog = new() { Title = title };
                 var result = await dialog.ShowAsync(View);
                 return result?[0];
