efdca8b9eb949e82fc85a828668df6a09c84ce89
ArchLeaders <archparliament@gmail.com>
2022-08-28 14:36:55 -0700

    Create application meta data

---
+++ BotwAvaloniaTemplate/Meta.cs
@@ -0,0 +1,9 @@
+﻿namespace BotwAvaloniaTemplate
+{
+    internal static class Meta
+    {
+        public static string Version { get; } = "0.1.0-alpha";
+        public static string BaseUrl { get; } = "https://raw.githubusercontent.com/ArchLeaders/BotwAvaloniaTemplate/master";
+        public static string ToCommonPath(this string path) => path.Replace("\\", "/");
+    }
+}
