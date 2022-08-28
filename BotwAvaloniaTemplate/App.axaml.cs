96acf530a14ab866ed471ac96affc81f6e2d799b
ArchLeaders <archparliament@gmail.com>
2022-08-28 01:49:24 -0700

    Run confg loader and setter on App startup

--- BotwAvaloniaTemplate/App.axaml.cs
+++ BotwAvaloniaTemplate/App.axaml.cs
@@ -1,24 +1,52 @@
+global using static BotwAvaloniaTemplate.App;
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
+using Avalonia.Themes.Fluent;
+using BotwAvaloniaTemplate.Extensions;
 using BotwAvaloniaTemplate.ViewModels;
 using BotwAvaloniaTemplate.Views;
+using Material.Icons;
+using System;
+using System.Threading.Tasks;
 
 namespace BotwAvaloniaTemplate
 {
     public partial class App : Application
     {
-        public override void Initialize()
-        {
-            AvaloniaXamlLoader.Load(this);
-        }
+        public static AppView View { get; private set; } = null!;
+        public static FluentTheme Theme { get; set; } = new(new Uri("avares://BotwActorTool.GUI/Styles"));
 
-        public override void OnFrameworkInitializationCompleted()
+        public override void Initialize() => AvaloniaXamlLoader.Load(this);
+        public override async void OnFrameworkInitializationCompleted()
         {
+            Config.LoadConfig();
+
+            Theme.Mode = Config.Theme == "Dark" ? FluentThemeMode.Dark : FluentThemeMode.Light;
+            Current!.Styles[0] = Theme;
+
             if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop) {
-                desktop.MainWindow = new MainWindow {
-                    DataContext = new MainWindowViewModel(),
-                };
+
+                // Create desktop instance
+                View = new();
+                desktop.MainWindow = View;
+
+                // Create data context
+                AppViewModel dataContext = new();
+                View.DataContext = dataContext;
+
+                // Make sure settings are always set
+                if (Config.IsNull) {
+                    ((AppViewModel)View.DataContext).SettingsView = new(canClose: false);
+                    ((AppViewModel)View.DataContext).SetStatus("Waiting for settings input", MaterialIconKind.BoxVariant);
+
+                    await Task.Run(() => {
+                        while (Config.IsNull)
+                            Task.Delay(100);
+                    });
+
+                    ((AppViewModel)View.DataContext).SetStatus();
+                }
             }
 
             base.OnFrameworkInitializationCompleted();
